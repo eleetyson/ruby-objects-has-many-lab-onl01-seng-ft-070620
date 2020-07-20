b3238881c18ec432942cd5b35c958867b8429a26
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-20 20:05:00 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,24 @@
+require 'pry'
+class Post
+  attr_accessor :author, :title
+  
+  @@all = []
+  
+  def initialize(title)
+    @title = title
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def author_name
+    
+  end
+  
+  def self.all
+    @@all  
+  end
+  
+end
